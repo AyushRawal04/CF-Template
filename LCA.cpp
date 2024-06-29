addb14edc0ab41ab396abe101f09446a428944ec
Ayush Rawal <114899729+AyushRawal04@users.noreply.github.com>
2024-06-29 23:55:11 +0530

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,4 +1,4 @@
-void dfs(int node, int parent, vector<vi> &adj, vector<vi>&par, vi &lev){
+void dfs(int node, int parent, vector<vector<int>> &adj, vector<vector<int>>&par, vector<int> &lev){
     par[0][node]=parent;
     if(parent != -1 ? lev[node]=lev[parent]+1 : lev[node]=1);
     for(auto& x: adj[node]){
@@ -8,7 +8,7 @@ void dfs(int node, int parent, vector<vi> &adj, vector<vi>&par, vi &lev){
     }
 }
 
-int findkth(int a, int k, vector<vi>& par){
+int findkth(int a, int k, vector<vector<int>>& par){
     int n=par.size();
     for(int i=0; i<n; i++){
         if(k&(1<<i)){
@@ -21,8 +21,8 @@ int findkth(int a, int k, vector<vi>& par){
     return a;
 }
 
-int lca(int a, int b, vector<vi> &par, vi &lev){
-    int n=par.size();
+int lca(int a, int b, vector<vector<int>> &par, vector<int> &lev){
+    int maxpow=par.size();
     if(lev[a]>lev[b]){
         a=findkth(a, lev[a]-lev[b], par);
     }else{
@@ -31,13 +31,26 @@ int lca(int a, int b, vector<vi> &par, vi &lev){
     if(a==b){
         return a;
     }
-    debug(a);
-    debug(b);
-    for(int i=n-1; i>=0; i--){
+    for(int i=maxpow-1; i>=0; i--){
         if(par[i][a]!=par[i][b]){
             a=par[i][a];
             b=par[i][b];
         }
     }
     return par[0][a];
+}
+
+void ConstructParent(vector<vector<int>> &par){
+    int maxpow=par.size();
+    int n=par[0].size()-1;
+    for(int i=1; i<maxpow; i++){
+        for(int j=1; j<=n; j++){
+            int imd=par[i-1][j];
+            if(imd==-1){
+                par[i][j]=-1;
+                continue;
+            }
+            par[i][j]=par[i-1][imd];
+        }
+    }
 }
